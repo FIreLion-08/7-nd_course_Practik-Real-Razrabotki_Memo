98e23290dc8011fa9da35bb3df514f6e10a454be
FIreLion-08 <Works-dim@yandex.ru>
2025-05-23 08:10:06 +0800

    Zadacha_02_v11_Functional_Log_Reg_Header

--- src/pages/AuthPage/AuthPage.styled.js
+++ src/pages/AuthPage/AuthPage.styled.js
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom'
 
 export const Container = styled.div`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
     background-color: #f5f5f5;
     gap: 20px;
+
+    @media screen and (max-width: 375px) {
+        width: 100%;
+        background-color: #fff;
+        /* padding: 0 16px; */
+    }
 `
 
 export const Card = styled.div`
@@ -17,7 +24,15 @@ export const Card = styled.div`
     box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
     width: 100%;
     max-width: 500px;
-    
+    @media screen and (max-width: 375px) {
+        width: 100%;
+        box-shadow: 0 4px 12px #fff;
+        /* height: 100%; */
+        display: grid;
+        place-items: center;
+        border-radius: 0px;
+        /* padding: 0 16px; */
+    }
 `
 
 export const Title = styled.h2`
